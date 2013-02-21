ae1563513870797e0360e54621ff1382a2f089fc
Christian G. Warden <cwarden@xerus.org>
2013-02-21 12:05:24 -0800

    Make remote XHR request on load

--- www/js/index.js
+++ www/js/index.js
@@ -30,20 +30,14 @@ var app = {
 	},
 	// deviceready Event Handler
 	//
-	// The scope of 'this' is the event. In order to call the 'receivedEvent'
-	// function, we must explicity call 'app.receivedEvent(...);'
 	onDeviceReady: function() {
-		app.receivedEvent('deviceready');
+		$.ajax("http://bookne.ws/ajax/example", {
+			type: "POST",
+			dataType: 'json',
+		}).done(function(data) {
+			$.each(data, function(i) {
+				$("<img/>").attr("src", 'http://placekitten.com/' + i*100, i*100).appendTo("#images");
+			});
+		});
 	},
-	// Update DOM on a Received Event
-	receivedEvent: function(id) {
-		var parentElement = document.getElementById(id);
-		var listeningElement = parentElement.querySelector('.listening');
-		var receivedElement = parentElement.querySelector('.received');
-
-		listeningElement.setAttribute('style', 'display:none;');
-		receivedElement.setAttribute('style', 'display:block;');
-
-		console.log('Received Event: ' + id);
-	}
 };
